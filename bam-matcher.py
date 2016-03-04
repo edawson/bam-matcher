6953ed7e091142d6c22e58149bdadb34f0cae459
Paul Wang <ppswang@gmail.com>
2016-03-04 19:04:10 +1030

    disabled experimental features in preparation for a branch splitting

--- bam-matcher.py
+++ bam-matcher.py
@@ -28,7 +28,7 @@ from fisher import pvalue
 from bammatcher_methods import *
 
 # EXPERIMENTAL METHODS
-from bammatcher_exp import *
+# from bammatcher_exp import *
 
 #============================================================================
 def handle_args():
@@ -116,10 +116,10 @@ def handle_args():
                              help="Specify directory for cached data. Overrides configuration")
 
     # Experimental features
-    parser_grp8 = parser.add_argument_group("EXPERIMENTAL")
-    parser_grp8.add_argument("--allele-freq",    "-AF", required=False,
-                             default=False, action="store_true",
-                             help="Plot variant allele frequency graphs")
+    # parser_grp8 = parser.add_argument_group("EXPERIMENTAL")
+    # parser_grp8.add_argument("--allele-freq",    "-AF", required=False,
+    #                          default=False, action="store_true",
+    #                          help="Plot variant allele frequency graphs")
 
     # optional, not in config
     parser.add_argument("--debug", "-d", required=False, action="store_true",
@@ -1626,13 +1626,13 @@ fout.close()
 
 # generate allele frequency distribution
 
-bin_count = 50
-vaf_txt_fig = []
-if args.allele_freq:
-    for i in [0,1]:
-        fig_out = REPORT_PATH + "_bam%d_vaf.png" % (i+1)
-        bam_aflist, bam_afbins = count_AF_bins(tsv_list[i], bin_count)
-        plot_VAF(bam_aflist, bin_count, fig_out, bam_list[i])
+# bin_count = 50
+# vaf_txt_fig = []
+# if args.allele_freq:
+#     for i in [0,1]:
+#         fig_out = REPORT_PATH + "_bam%d_vaf.png" % (i+1)
+#         bam_aflist, bam_afbins = count_AF_bins(tsv_list[i], bin_count)
+#         plot_VAF(bam_aflist, bin_count, fig_out, bam_list[i])
 #        aplot = plot_ascii_VAF(bam_aflist)
 
 
